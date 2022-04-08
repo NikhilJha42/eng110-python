67e49502a84c60bbf330c4ddb09dd127c9e0ecb2
Nikhil Jha <njha@spartaglobal.com>
2022-04-08 11:30:24 +0100

    Update to variables_intro and created string_intro

---
+++ variables/variables_intro.py
@@ -0,0 +1,26 @@
+# a = 1
+# b = 2
+# c = 3.5
+# d = "hello"
+
+# Dynamically Typed Language
+
+# Numeric Types
+
+# a = 125 # int
+# b = 1.25 # float
+# c = 3 + 4j # complex
+
+# What data types result from combining ints and/or floats with each operator?
+
+# x1 = 2 + 1j
+# x2 = 2 - 1j
+# x3 = x1*x2
+#
+# print("Complex numbers:", x3, type(x3))
+#
+# y = 20 - 34
+# print(y, type(y))
+# x = 4 + 2j
+# y = x % 5
+# print(y)
