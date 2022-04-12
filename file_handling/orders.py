d1a67597a4638a7088e2ec27c782493c4eeb2b36
Nikhil Jha <njha@spartaglobal.com>
2022-04-12 14:09:16 +0100

    Added personal_details.txt

--- file_handling/orders.py
+++ file_handling/orders.py
@@ -1,15 +1,23 @@
 def  open_and_print_file(file):
     try:
-        opened_file = open(file, "r")
-        file_line_list = opened_file.readlines()
+        with open(file, "r") as file:
+            for line in file.readlines():
+                print(line.rstrip('\n'))
 
-        for line in file_line_list:
-            print(line.rstrip("\n"))
+        # opened_file = open(file, "r")
+        # file_line_list = opened_file.readlines()
+        #
+        # print(file_line_list)
+        #
+        # for line in file_line_list:
+        #     print(line.rstrip("\n"))
+        #
+        # opened_file.close()
 
-        opened_file.close()
-    
     except FileNameNotFoundError:
         print("File cannot be found or directory is wrong")
         raise
+    finally:
+        print("\n--Execution complete")
 
-open_and_print_file("order.txt")
+open_and_print_file("orders.txt")
